23f76b15ec821abef1b4dc441e7b5be032f30327
pandora1881 <igor.migalushhhh@gmail.com>
2023-05-15 14:29:29 +0300

    lab3

---
+++ 1_2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a, c, b;
+
+    printf("Введіть ціле число a: ");
+    scanf("%d", &a);
+
+    printf("Введіть ціле число c: ");
+    scanf("%d", &c);
+
+    if (c < 5)
+    {
+        b = a * a - 4 * c / a;
+    }
+    if (c >= 5 && c < 10)
+    {
+        b = sqrt(a * c - 8 * a);
+    }
+    if (c > 10)
+    {
+        b = c * c - 2 * a;
+    }
+
+    printf("b = %d\n", b);
+
+    return 0;
+}
